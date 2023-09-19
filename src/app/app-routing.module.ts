cb7357ecb533728f26b0f03a4032d7020e8f0668
Rossywan França <rossyfranca@gmail.com>
2023-09-19 10:19:19 -0300

    finalizing implementations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { DetailComponent } from './pages/detail/detail.component';
+
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'detail/:id', component:DetailComponent}
+  {path:'',component:HomeComponent}
 ];
 
 @NgModule({
